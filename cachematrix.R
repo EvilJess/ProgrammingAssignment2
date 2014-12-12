a5131bb28b78ac7ec129ca49046e920469a29a19
EvilJess <j.m.lethbridge@gmail.com>
2014-12-12 14:18:00 +1100

    fixed mistake in cacheSolve, missing () on inv <- x()

--- cachematrix.R
+++ cachematrix.R
@@ -32,7 +32,7 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
     
-    inv <- x$getinverse
+    inv <- x$getinverse()
     
     if (!is.null(inv)) {
         message("getting cached data")
